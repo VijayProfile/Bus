a4eab8941c1d100fdfbb33b701e34a22e68ceed8
VijayProfile <kathirvijay28@gmail.com>
2025-04-21 16:14:33 +0530

    Auto-generated commit

--- chunk-MUD73GLV.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-UDFCU2GF.js";import"./chunk-VP7RTXBD.js";import"./chunk-RZ4AUNZR.js";export{a as FooterComponent};
---
+++ chunk-VNX3XIU5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-J36M6UZZ.js";import"./chunk-VP7RTXBD.js";import"./chunk-RZ4AUNZR.js";export{a as FooterComponent};
